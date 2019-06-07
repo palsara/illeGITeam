f3e4e1a92625feabde3d5aff13937eb7920ce846
George Komor <51327901+Veneration218@users.noreply.github.com>
2019-06-07 15:45:03 +0200

    Add files via upload
    added namegenerator.js

---
+++ name-generator.js
@@ -0,0 +1,15 @@
+function generator() {
+    var monsterNames = ["Múmia", "Kísértet", "Csontváz", "Zombi", "Démon"]
+    var adjectives = [", a gonosz", ", a kísértő", ", a félelmetes", ", a sötét varázsigék tudója", ", a szexi"]
+
+    var randomNumber1 = parseInt(Math.random() * monsterNames.length);
+    var randomNumber2 = parseInt(Math.random() * adjectives.length);
+    var fullName = monsterNames[randomNumber1] + adjectives[randomNumber2];
+
+    alert(fullName);
+
+    //ha már kidobott egy nevet, azt kitörli
+    if (document.getElementById("result")) {
+        document.getElementById("placeholder").removeChild(document.getElementById("result"));
+    }
+}

a2dba21641d989a3615a1cdc6617b9b3f680cbe1
Daniel Olah <olah.daniel95@gmail.com>
2019-06-07 14:24:50 +0200

    Added first set of questions

---
+++ js/randomQuit.js
@@ -0,0 +1,143 @@
+var quizQuestions = [
+    // First question block
+    [{
+            question = 'Hogy hívják a Pink Floyd eredeti énekesét?',
+            answer = 'Syd Barrett',
+            image = '<img src="/img/sydBarrett.jpg">'
+        },
+        {
+            question = 'Milyen betegségben szenvedett?',
+            answer = 'Skizofrénia',
+            false1 = 'AIDS',
+            false2 = 'Alzheimer-kór',
+            false3 = 'Parkinson-kór',
+            false4 = 'Bőrrak',
+        },
+        {
+            question = "Melyik tudatmódosító szerrel élt a leggyakrabban?",
+            answer = "LSD",
+            false1 = "DMT",
+            false2 = "Kokain",
+            false3 = "Marijuana",
+            false4 = "Alkohol",
+        },
+        {
+            question = "Hogy hívják az utódját, akivel végül világhírre tettek szert?",
+            answer = "David Gilmour",
+            false1 = "Syd Vicious",
+            false2 = "Lemmy Kilmister",
+            false3 = "Ozzy Osbourne",
+            false4 = "Ivan Moody",
+        },
+        {
+            question = "Melyik albumuk (eladott lemezek száma alapján) a legsikeresebb?",
+            answer = "The Dark Side of the Moon",
+            false1 = "Wish You Were Here",
+            false2 = "The Division Bell",
+            false3 = "The Wall",
+            false4 = "The Piper at the Gates of Dawn",
+        },
+    ],
+    // Second question block
+    [{
+        question = "",
+        answer = "",
+
+    }, {
+        question = "",
+        answer = "",
+        false1 = "",
+        false2 = "",
+        false3 = "",
+        false4 = "",
+    }, {
+        question = "",
+        answer = "",
+        false1 = "",
+        false2 = "",
+        false3 = "",
+        false4 = "",
+    }, {
+        question = "",
+        answer = "",
+        false1 = "",
+        false2 = "",
+        false3 = "",
+        false4 = "",
+    }, {
+        question = "",
+        answer = "",
+        false1 = "",
+        false2 = "",
+        false3 = "",
+        false4 = "",
+    }, ],
+    // Third question block
+    [{
+        question = "",
+        answer = "",
+
+    }, {
+        question = "",
+        answer = "",
+        false1 = "",
+        false2 = "",
+        false3 = "",
+        false4 = "",
+    }, {
+        question = "",
+        answer = "",
+        false1 = "",
+        false2 = "",
+        false3 = "",
+        false4 = "",
+    }, {
+        question = "",
+        answer = "",
+        false1 = "",
+        false2 = "",
+        false3 = "",
+        false4 = "",
+    }, {
+        question = "",
+        answer = "",
+        false1 = "",
+        false2 = "",
+        false3 = "",
+        false4 = "",
+    }, ],
+    // Fourth question block
+    [{
+        question = "",
+        answer = "",
+
+    }, {
+        question = "",
+        answer = "",
+        false1 = "",
+        false2 = "",
+        false3 = "",
+        false4 = "",
+    }, {
+        question = "",
+        answer = "",
+        false1 = "",
+        false2 = "",
+        false3 = "",
+        false4 = "",
+    }, {
+        question = "",
+        answer = "",
+        false1 = "",
+        false2 = "",
+        false3 = "",
+        false4 = "",
+    }, {
+        question = "",
+        answer = "",
+        false1 = "",
+        false2 = "",
+        false3 = "",
+        false4 = "",
+    }, ],
+];

6fd0199675f026e82294952de001a51263318851
Kosoczki Eva <eva.kosoczki@gmail.com>
2019-06-09 04:14:26 +0200

    css index modified

--- js/picker.js
+++ js/picker.js
@@ -7,14 +7,12 @@ function Picker() {
     var funny = isfunny.checked;
 
     if (magyar == 1) {
-        document.querySelector("#movie").innerHTML = "Lássuk a medvét: Magyar film";
         var btn = document.createElement('a');
         btn.href = "https://www.google.com/"
         btn.innerHTML = "Magyar";
         document.body.appendChild(btn);
     } else {
         if (scary == 1) {
-            document.querySelector("#movie").innerHTML = "Lássuk a medvét: Horror";
             var btn = document.createElement('a');
             btn.href = "https://www.google.com/"
             btn.innerHTML = "Horror";
@@ -22,13 +20,11 @@ function Picker() {
 
         } else {
             if (funny == 1) {
-                document.querySelector("#movie").innerHTML = "Lássuk a medvét: ZS kategória";
                 var btn = document.createElement('a');
                 btn.href = "https://www.google.com/"
                 btn.innerHTML = "Zs";
                 document.body.appendChild(btn);
             } else {
-                document.querySelector("#movie").innerHTML = "Lássuk a medvét: SciFi";
                 var btn = document.createElement('a');
                 btn.href = "https://www.google.com/"
                 btn.innerHTML = "SciFi";
